cf0be9870c95e6b8a7cc563d6dc9324298fb967b
Zulaykha <suliatsulaiman@gmail.com>
2025-04-07 13:39:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+
+          <div className="content">
+            <h1> Hello World</h1>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default App
